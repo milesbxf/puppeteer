95004ddcf470ca57f0d022dd5c489abb0dc2f669
Miles Bryant <milesbryant@monzo.com>
2019-05-05 22:15:53 +0100

    Refactor pipeline e2e tests

---
+++ e2e/core/v1alpha1/helpers_test.go
@@ -0,0 +1,149 @@
+package v1alpha1_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/milesbxf/puppeteer/e2e"
+	corev1alpha1 "github.com/milesbxf/puppeteer/pkg/apis/core/v1alpha1"
+	"github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func SetUpPipeline(t *testing.T, rig *e2e.TestRig, name string) (*corev1alpha1.PipelineConfig, *corev1alpha1.Pipeline) {
+	g := gomega.NewGomegaWithT(t)
+
+	config_file := name + "_config.yaml"
+	pipeline_file := name + ".yaml"
+
+	// Load and create a simple Pipeline Config
+
+	objs, err := e2e.LoadResourcesFromTestData(config_file)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "loading pipeline config yaml")
+
+	pipelineConfig, ok := objs[0].(*corev1alpha1.PipelineConfig)
+	pipelineConfig.ObjectMeta.Namespace = rig.Namespace
+	g.Expect(ok).To(gomega.BeTrue(), "loading pipeline config obj")
+	err = rig.K8s.Create(context.TODO(), pipelineConfig)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "creating pipeline config obj")
+	t.Logf("Created pipeline object %s", pipelineConfig.Name)
+
+	// Manually trigger a Pipeline
+
+	objs, err = e2e.LoadResourcesFromTestData(pipeline_file)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "loading pipeline yaml")
+
+	pipeline, ok := objs[0].(*corev1alpha1.Pipeline)
+	g.Expect(ok).To(gomega.BeTrue(), "loading pipeline obj")
+
+	pipeline.ObjectMeta.Namespace = rig.Namespace
+
+	err = rig.K8s.Create(context.TODO(), pipeline)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "creating pipeline obj")
+
+	g.Eventually(func() error {
+		p := &corev1alpha1.Pipeline{}
+		return rig.K8s.Get(context.Background(), types.NamespacedName{Name: pipeline.Name, Namespace: rig.Namespace}, p)
+	}, "5s").Should(gomega.Succeed(), "creating pipeline obj")
+	t.Logf("Created pipeline object %s", pipeline.Name)
+
+	return pipelineConfig, pipeline
+}
+
+func GetArtifactFromPipeline(c client.Client, name, namespace string) (*corev1alpha1.PipelineArtifact, error) {
+	p := &corev1alpha1.Pipeline{}
+	err := c.Get(context.Background(), types.NamespacedName{Name: name, Namespace: namespace}, p)
+	if err != nil {
+		return nil, err
+	}
+	return p.Spec.Inputs["scm-upstream"].Artifact, err
+}
+
+func WaitForPipelineArtifactName(t *testing.T, rig *e2e.TestRig, pipeline *corev1alpha1.Pipeline) string {
+	g := gomega.NewGomegaWithT(t)
+
+	g.Eventually(func() (*corev1alpha1.PipelineArtifact, error) {
+		return GetArtifactFromPipeline(rig.K8s, pipeline.Name, rig.Namespace)
+	}, "30s").
+		ShouldNot(gomega.BeNil(), "waiting for pipeline artifact name")
+
+	a, err := GetArtifactFromPipeline(rig.K8s, pipeline.Name, rig.Namespace)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "getting artifact from pipeline")
+	t.Logf("Got name of artifact for pipeline input: '%s'", a.Name)
+
+	return a.Name
+}
+
+func WaitForArtifact(t *testing.T, rig *e2e.TestRig, artifactName string) *corev1alpha1.Artifact {
+	g := gomega.NewGomegaWithT(t)
+
+	g.Eventually(func() error {
+		artifact := &corev1alpha1.Artifact{}
+		return rig.K8s.Get(context.Background(), types.NamespacedName{Name: artifactName, Namespace: rig.Namespace}, artifact)
+	}, "30s").
+		Should(gomega.Succeed(), "waiting for artifact")
+
+	artifact := &corev1alpha1.Artifact{}
+	err := rig.K8s.Get(context.Background(), types.NamespacedName{Name: artifactName, Namespace: rig.Namespace}, artifact)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "getting artifact")
+	t.Logf("Got artifact for pipeline instance input: '%s'", artifact.Name)
+	return artifact
+}
+
+func GetStorageReferenceFromArtifact(c client.Client, name, namespace string) (*corev1alpha1.StorageReference, error) {
+	artifact := &corev1alpha1.Artifact{}
+	err := c.Get(context.Background(), types.NamespacedName{Name: name, Namespace: namespace}, artifact)
+	if err != nil {
+		return nil, err
+	}
+	return artifact.Status.Reference, nil
+
+}
+
+func WaitForArtifactStorageReference(t *testing.T, rig *e2e.TestRig, artifactName string) *corev1alpha1.StorageReference {
+	g := gomega.NewGomegaWithT(t)
+
+	g.Eventually(func() (*corev1alpha1.StorageReference, error) {
+		return GetStorageReferenceFromArtifact(rig.K8s, artifactName, rig.Namespace)
+	}, "2m").
+		ShouldNot(gomega.BeNil(), "getting artifact reference")
+
+	sr, err := GetStorageReferenceFromArtifact(rig.K8s, artifactName, rig.Namespace)
+	g.Expect(err).NotTo(gomega.HaveOccurred(), "getting storage reference")
+	return sr
+}
+
+func WaitForStagePhase(t *testing.T, rig *e2e.TestRig, stageName string, phase corev1alpha1.StagePhase) {
+	g := gomega.NewGomegaWithT(t)
+
+	g.Eventually(
+		func() (corev1alpha1.StagePhase, error) {
+			stage := &corev1alpha1.Stage{}
+			err := rig.K8s.Get(context.Background(), types.NamespacedName{Name: stageName, Namespace: rig.Namespace}, stage)
+			if err != nil {
+				return "", err
+			}
+			return stage.Status.Phase, nil
+		},
+		"30s",
+	).Should(gomega.Equal(phase), "waiting for pipeline stage "+string(phase))
+	t.Logf("Pipeline stage is %s", phase)
+}
+
+func WaitForTaskPhase(t *testing.T, rig *e2e.TestRig, taskName string, phase corev1alpha1.TaskPhase) {
+	g := gomega.NewGomegaWithT(t)
+
+	g.Eventually(
+		func() (corev1alpha1.TaskPhase, error) {
+			task := &corev1alpha1.Task{}
+			err := rig.K8s.Get(context.Background(), types.NamespacedName{Name: taskName, Namespace: rig.Namespace}, task)
+			if err != nil {
+				return "", err
+			}
+			return task.Status.Phase, nil
+		},
+		"30s",
+	).Should(gomega.Equal(phase), "waiting for pipeline task "+string(phase))
+	t.Logf("Pipeline task is %s", phase)
+}
--- e2e/core/v1alpha1/pipeline_test.go
+++ e2e/core/v1alpha1/pipeline_test.go
@@ -1,14 +1,11 @@
 package v1alpha1_test
 
 import (
-	"context"
 	"testing"
 
 	"github.com/milesbxf/puppeteer/e2e"
 	corev1alpha1 "github.com/milesbxf/puppeteer/pkg/apis/core/v1alpha1"
 	"github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestSimpleBuildPipeline(t *testing.T) {
@@ -19,108 +16,20 @@ func TestSimpleBuildPipeline(t *testing.T) {
 	g.Expect(err).NotTo(gomega.HaveOccurred(), "setting up test rig")
 	t.Logf("Test rig set up with namespace %s", rig.Namespace)
 
-	// Load and create a simple Pipeline Config
-
-	objs, err := e2e.LoadResourcesFromTestData("simple_build_pipeline_config.yaml")
-	g.Expect(err).NotTo(gomega.HaveOccurred(), "loading pipeline config yaml")
-
-	pipelineConfig, ok := objs[0].(*corev1alpha1.PipelineConfig)
-	pipelineConfig.ObjectMeta.Namespace = rig.Namespace
-	g.Expect(ok).To(gomega.BeTrue(), "loading pipeline config obj")
-	err = rig.K8s.Create(context.TODO(), pipelineConfig)
-	g.Expect(err).NotTo(gomega.HaveOccurred(), "creating pipeline config obj")
-	t.Logf("Created pipeline object %s", pipelineConfig.Name)
-
-	// Manually trigger a Pipeline
-
-	objs, err = e2e.LoadResourcesFromTestData("simple_build_pipeline.yaml")
-	g.Expect(err).NotTo(gomega.HaveOccurred(), "loading pipeline yaml")
-
-	pipeline, ok := objs[0].(*corev1alpha1.Pipeline)
-	g.Expect(ok).To(gomega.BeTrue(), "loading pipeline obj")
-
-	pipeline.ObjectMeta.Namespace = rig.Namespace
-
-	err = rig.K8s.Create(context.TODO(), pipeline)
-	g.Expect(err).NotTo(gomega.HaveOccurred(), "creating pipeline obj")
-
-	g.Eventually(func() error {
-		p := &corev1alpha1.Pipeline{}
-		return rig.K8s.Get(context.Background(), types.NamespacedName{Name: pipeline.Name, Namespace: rig.Namespace}, p)
-	}, "5s").Should(gomega.Succeed(), "creating pipeline obj")
-	t.Logf("Created pipeline object %s", pipeline.Name)
+	_, pipeline := SetUpPipeline(t, rig, "simple_pipeline")
 
 	// Now the pipeline instance controller reconciles. It wants to associate the instance
 	// with an Artifact matching the config provided. If there isn't already one matching the Source,
 	// it creates one, without filling in the reference.
 
-	g.Eventually(
-		func() (*corev1alpha1.PipelineArtifact, error) {
-			return getArtifactFromPipeline(rig.K8s, pipeline.Name, rig.Namespace)
-		},
-		"30s",
-	).ShouldNot(gomega.BeNil(), "waiting for pipeline artifact")
+	artifactName := WaitForPipelineArtifactName(t, rig, pipeline)
 
-	a, err := getArtifactFromPipeline(rig.K8s, pipeline.Name, rig.Namespace)
-	g.Expect(err).NotTo(gomega.HaveOccurred(), "getting artifact from pipeline")
-	t.Logf("Got name of artifact for pipeline input: '%s'", a.Name)
+	WaitForArtifact(t, rig, artifactName)
+	WaitForArtifactStorageReference(t, rig, artifactName)
 
-	g.Eventually(
-		func() (*corev1alpha1.StorageReference, error) {
-			artifact := &corev1alpha1.Artifact{}
-			err = rig.K8s.Get(context.Background(), types.NamespacedName{Name: a.Name, Namespace: rig.Namespace}, artifact)
-			if err != nil {
-				return nil, err
-			}
-			return artifact.Status.Reference, nil
-		},
-		"2m",
-	).ShouldNot(gomega.BeNil(), "getting artifact reference")
+	WaitForStagePhase(t, rig, pipeline.Name+"-build-1", corev1alpha1.StageInProgress)
+	WaitForTaskPhase(t, rig, pipeline.Name+"-build-1-build-image-1", corev1alpha1.TaskInProgress)
 
-	artifact := &corev1alpha1.Artifact{}
-	err = rig.K8s.Get(context.Background(), types.NamespacedName{Name: a.Name, Namespace: rig.Namespace}, artifact)
-	g.Expect(err).NotTo(gomega.HaveOccurred(), "getting artifact")
-	t.Logf("Got artifact for pipeline instance input: '%s'", artifact.Name)
-
-	g.Eventually(
-		func() (corev1alpha1.StagePhase, error) {
-			stageInstance := &corev1alpha1.Stage{}
-			err = rig.K8s.Get(context.Background(), types.NamespacedName{Name: pipeline.Name + "-build-1", Namespace: rig.Namespace}, stageInstance)
-			if err != nil {
-				return "", err
-			}
-			return stageInstance.Status.Phase, nil
-		},
-		"30s",
-	).Should(gomega.Equal(corev1alpha1.StageInProgress), "waiting for pipeline stage in progress")
-	t.Logf("Pipeline stage is in progress")
-
-	g.Eventually(
-		func() (corev1alpha1.TaskPhase, error) {
-			stageInstance := &corev1alpha1.Task{}
-			err = rig.K8s.Get(context.Background(), types.NamespacedName{Name: pipeline.Name + "-build-1-build-image-1", Namespace: rig.Namespace}, stageInstance)
-			if err != nil {
-				return "", err
-			}
-			return stageInstance.Status.Phase, nil
-		},
-		"30s",
-	).Should(gomega.Equal(corev1alpha1.TaskInProgress), "waiting for task in progress")
-	t.Logf("Task is in progress")
-	// Spins up job with configured image and shell script
-	// Build sidecar pulls local storage down into a shared volume
-	// Waits for attached job to finish
-	// Searches for outputs
-	//   In this case we have a Docker image - it gets saved as a tarball and put in local storage
-	//   Another artifact is created
-
-}
-
-func getArtifactFromPipeline(c client.Client, name, namespace string) (*corev1alpha1.PipelineArtifact, error) {
-	p := &corev1alpha1.Pipeline{}
-	err := c.Get(context.Background(), types.NamespacedName{Name: name, Namespace: namespace}, p)
-	if err != nil {
-		return nil, err
-	}
-	return p.Spec.Inputs["scm-upstream"].Artifact, err
+	WaitForTaskPhase(t, rig, pipeline.Name+"-build-1-build-image-1", corev1alpha1.TaskComplete)
+	WaitForStagePhase(t, rig, pipeline.Name+"-build-1", corev1alpha1.StageComplete)
 }
